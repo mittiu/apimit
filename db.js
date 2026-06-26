9102321e42a01050abec2122ac353f13912c45b7
mittiu <65239835+mittiu@users.noreply.github.com>
2026-06-26 00:01:47 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,11 @@
-const mysql = require('mysql2/promise');
-require('dotenv').config();
+const mysql = require("mysql2/promise");
+require("dotenv").config();
 
-const pool = mysql.createPool(process.env.DATABASE_URL);
+const pool = mysql.createPool({
+  uri: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 module.exports = pool;
